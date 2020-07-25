e12b3f0431a7d2f72c7995198a36516bf6ed9f98
root <charlespogzz@googlemail.com>
2020-07-25 15:55:51 -0400

    commit1

---
+++ terra.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    profile = "default"
+    region = "us-west-2"
+    AWS_ACCESS_KEY_ID={$AWS_ACCESS_KEY_ID}
+    AWS_SECRET_ACCESS_KEY={$AWS_SECRET_ACCESS_KEY}
+
+}
+
+resource "aws_s3_bucket" "tf_course" {
+    bucket = "tf-course-20200726"
+    acl = "private"
+  
+}
