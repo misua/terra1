3428b65b9e9875135ddaf48837228c44502eb945
misua <charlespogzz@googlemail.com>
2020-08-02 06:41:15 +0800

    Use eip assoc and scale up web instances

--- terra.tf
+++ terra.tf
@@ -45,6 +45,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+    count = 2
+
     ami = "ami-01ebd27ddd89835f4"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.prod_web.id]
@@ -55,9 +57,13 @@ resource "aws_instance" "prod_web" {
     
 }
 
+resource "aws_eip_association" "prod_web" {
+    instance_id = aws_instance.prod_web[0].id
+    allocation_id = aws_eip.prod_web.id
+  
+}
+
 resource "aws_eip" "prod_web" {
-    instance = aws_instance.prod_web.id
-    
 
     tags = {
       "Terraform" = "true"
