4e8396a7098bde4b7142d052455a8f670546636d
misua <charlespogzz@googlemail.com>
2020-08-02 04:37:30 +0800

    adding security and vpc

--- terra.tf
+++ terra.tf
@@ -1,12 +1,46 @@
 provider "aws" {
-    profile = "sabsab"
+    profile = "default"
     region = "us-west-2"
     
     
 }
 
-resource "aws_s3_bucket" "tf_course" {
-    bucket = "tf-course-20200726"
+resource "aws_s3_bucket" "prod_tf_course" {
+    bucket = "tf-course-20200208"
     acl = "private"
+  
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+    name = "prod_web"
+    description = "Allow standard http/https inbound and outbound"
+
+    ingress  {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+     ingress  {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port =0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      "Terraform" = "true"
+    }
+
   
 }
